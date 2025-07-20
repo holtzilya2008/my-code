45e6a633764aea90336a265e39c2b464bd3e1cbd
Ilya Holtz <holtzilya2008@gmail.com>
2025-07-20 23:34:17 +0300

    Initial tests pass

--- topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/python_solution.py
+++ topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/python_solution.py
@@ -7,17 +7,29 @@ def find_longest_substring(input: str) -> int:
         return 0
     
     max = 1
-    current_substring_length = 1
+    window_start = 0
+    window_end = 0
+    window_chars_to_index = {input[0]: 0}
     
-    for index, char in enumerate(input):
-        if index == 0:
-            continue
-    
-        if input[index] == input[index - 1]:
-            current_substring_length = 1
-        else:
-            current_substring_length += 1
-            if current_substring_length > max: 
-                max = current_substring_length
-    
-    return max        
+    while window_end < last_index:
+        window_end += 1
+        next_char = input[window_end]
+        
+        if next_char in window_chars_to_index:
+            # Starting a new substring
+            previous_window_start = window_start
+            window_start = window_chars_to_index[next_char] + 1
+            
+            # Remove all unnecessary window eleemnts from the dict
+            for i in range(previous_window_start, window_start): 
+                window_chars_to_index.pop(input[i])
+            
+        # Add "next_char" to the window chars
+        window_chars_to_index[next_char] = window_end
+        window_size = len(window_chars_to_index)
+        
+        # Update the maximum
+        if window_size > max:
+            max = window_size
+            
+    return max
