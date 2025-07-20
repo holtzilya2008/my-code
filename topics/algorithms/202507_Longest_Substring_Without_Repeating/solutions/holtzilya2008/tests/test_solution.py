51aa62b9eea95fd37ecba579e52044ff5b6eaa5b
Ilya Holtz <holtzilya2008@gmail.com>
2025-07-20 23:41:17 +0300

    added more test cases

--- topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/tests/test_solution.py
+++ topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/tests/test_solution.py
@@ -5,7 +5,12 @@ from python_solution import find_longest_substring
 @pytest.mark.parametrize('input, expected_result', [
     ('abcabcbb', 3),
     ('bbbbb', 1),
-    ('pwwkew', 3)
+    ('pwwkew', 3),
+    ('a', 1),
+    ('', 0),
+    ('ab', 2),
+    ('aa', 1),
+    ('abcdefghijklmnopqrstuvwxyz0123456789a1', 36)
 ])
 def test_solution(input: str, expected_result: int):
     actual_result = find_longest_substring(input)
