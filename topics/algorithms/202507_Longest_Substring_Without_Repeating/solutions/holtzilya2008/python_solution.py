e4025e4f28d8e772ed9ba439bd3e50975fefc8ff
Ilya Holtz <holtzilya2008@gmail.com>
2025-07-20 21:48:09 +0300

    initial solution

--- topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/python_solution.py
+++ topics/algorithms/202507_Longest_Substring_Without_Repeating/solutions/holtzilya2008/python_solution.py
@@ -1,3 +1,23 @@
 
-def find_longest_substring(input: str) -> int:
-    raise NotImplementedError()
+def find_longest_substring(input: str) -> int:    
+    length = len(input)
+    last_index = length - 1
+    
+    if length == 0:
+        return 0
+    
+    max = 1
+    current_substring_length = 1
+    
+    for index, char in enumerate(input):
+        if index == 0:
+            continue
+    
+        if input[index] == input[index - 1]:
+            current_substring_length = 1
+        else:
+            current_substring_length += 1
+            if current_substring_length > max: 
+                max = current_substring_length
+    
+    return max        
